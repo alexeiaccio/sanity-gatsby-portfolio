505c092de5317020633436d7f24aa73b1fc0df25
Alexei Accio <alexei.accio@gmail.com>
2020-02-14 16:19:30 +0300

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,57 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-gatsby-portfolio'
+      }
+    },
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '5e469e5f71b65bdb79e068b3',
+                  title: 'Sanity Studio',
+                  name: 'sanity-gatsby-portfolio-studio-yv4f8fo2',
+                  apiId: 'c1f0ecea-499d-457f-bf00-3c51a9cdc8d7'
+                },
+                {
+                  buildHookId: '5e469e5f140b490b46daac34',
+                  title: 'Portfolio Website',
+                  name: 'sanity-gatsby-portfolio-web-9hwfy4zi',
+                  apiId: '0c6efe77-11e7-41e2-b540-78e4bf5c8943'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/alexeiaccio/sanity-gatsby-portfolio',
+            category: 'Code'
+          },
+          {
+            title: 'Frontend',
+            value: 'https://sanity-gatsby-portfolio-web-9hwfy4zi.netlify.com',
+            category: 'apps'
+          }
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent projects', order: '_createdAt desc', types: ['sampleProject']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'o9s7wk3s',
+    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+  }
+}
